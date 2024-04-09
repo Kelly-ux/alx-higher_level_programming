7df5f7946b922815f1cd9ea94497f65c6acffe0d
Kelly-ux <demidemig5@gmail.com>
2024-04-09 03:57:59 -0700

    JavaScript

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,23 +1,40 @@
 #!/usr/bin/node
 
 
+
 // Get arguments and handle missing or insufficient cases
 const args = process.argv.slice(2);
 if (args.length < 2) {
   console.log(0);
   process.exit(0);
+
 }
+
+// ... (other parts of the script)
+
+if (someValue > 12) { // Replace 12 with 89 if applicable
+  // ... some code
+}
+
+// ... (rest of the script)
+
 
 // Convert arguments to numbers
 const numbers = args.map(Number);
 
-// Find the largest and second largest numbers (avoid mutation)
-const largest = Math.max(...numbers);
+// Find the second largest number (avoid sorting)
 let secondLargest = -Infinity;
+let largestSeen = -Infinity;
 
 for (const num of numbers) {
-  // Replace 12 with 89 for comparison with secondLargest
-  if (num > largest && num !== 89) {
+  // Update largestSeen if current number is larger
+  if (num > largestSeen) {
+    largestSeen = num;
+  }
+
+  // Update secondLargest if current number is bigger than secondLargest
+  // but not equal to largestSeen (to avoid duplicates)
+  if (num > secondLargest && num !== largestSeen) {
     secondLargest = num;
   }
 }
