b78e05b86e0a6d0f573b908d670cafabf72355c2
Kelly-ux <demidemig5@gmail.com>
2024-04-09 00:19:46 -0700

    JavaScript

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
-console.log('C is cool')
-console.log('C is undefined')
-console.log('Undefined is undefined')
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Check if at least two arguments are provided
+if (!arg1 || !arg2) {
+  console.log("Error: Please provide two arguments");
+  process.exit(1);  // Exit with error code (optional)
+}
+
+console.log(`${arg1} is ${arg2}`);
+
